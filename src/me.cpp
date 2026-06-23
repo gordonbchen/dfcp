dbd2463e1fec51018b0f53463612a2c1f76c7894
gordonbchen <gordonbchen@gmail.com>
2026-06-23 16:45:23 -0400

    move tree into file

--- src/me.cpp
+++ src/me.cpp
@@ -1,4 +1,3 @@
-#include <cstdint>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -20,6 +19,7 @@
 #include "max.hpp"
 #include "expect.hpp"
 #include "elbo.hpp"
+#include "tree.hpp"
 #include "util.hpp"
 
 
@@ -27,62 +27,6 @@ void parse_double(char *s, double& x) {
     char* end_ptr = nullptr;
     x = std::strtod(s, &end_ptr);
     if (end_ptr == s) { throw std::invalid_argument("Failed to parse arg value double."); };
-}
-
-const char* parse_coal_tree(const char *s, std::unordered_map<int, std::tuple<int, int>>& coal_tree, int idx) {
-    while (*s != '(') { ++s; }
-    ++s;
-
-    int left_idx;
-    if (*s == '(') {
-        left_idx = -(2 * std::abs(idx));
-        s = parse_coal_tree(s, coal_tree, left_idx);
-    }
-    else {
-        left_idx = std::strtol(s, nullptr, 10) - 1;
-    }
-
-    while (*s != ' ') { ++s; }
-    ++s;
-
-    int right_idx;
-    if (*s == '(') {
-        right_idx = -(2*std::abs(idx) + 1);
-        s = parse_coal_tree(s, coal_tree, right_idx);
-    }
-    else {
-        right_idx = std::strtol(s, nullptr, 10) - 1;
-    }
-
-    coal_tree.emplace(idx, std::tuple<int, int>{left_idx, right_idx});
-    return s;
-}
-
-struct ParsimonyMsg {
-    int score;
-    uint64_t cluster_bm;
-};
-
-ParsimonyMsg calc_parsimony(
-    int idx, int l,
-    const std::unordered_map<int, std::tuple<int, int>>& coal_tree,
-    const Clusters& clusters, const std::unordered_map<Cluster*, int>& cluster_idxs
-) {
-    uint64_t cluster_bm = 0;
-    if (!coal_tree.contains(idx)) {
-        cluster_bm |= 1ULL << cluster_idxs.at(clusters.r_assign[idx2d(idx, l, clusters.HP.L)]);
-        return ParsimonyMsg{0, cluster_bm};
-    }
-
-    const auto& [left, right] = coal_tree.at(idx);
-    ParsimonyMsg lp = calc_parsimony(left, l, coal_tree, clusters, cluster_idxs);
-    ParsimonyMsg rp = calc_parsimony(right, l, coal_tree, clusters, cluster_idxs);
-
-    cluster_bm = lp.cluster_bm & rp.cluster_bm;
-    if (cluster_bm != 0) {
-        return ParsimonyMsg{lp.score + rp.score, cluster_bm};
-    }
-    return ParsimonyMsg{lp.score + rp.score + 1, lp.cluster_bm | rp.cluster_bm};
 }
 
 class EarlyStopping {
@@ -274,27 +218,8 @@ int main(int argc, char *argv[]) {
 
     // Tree parsimony.
     if (tree_file_name != nullptr) {
-        std::ifstream tree_file{tree_file_name};
-        if (!tree_file.is_open()) { throw std::runtime_error("Failed to open tree file."); };
-
-        std::string line;
-        for (int i = 0; i < 3; ++i) {
-            std::getline(tree_file, line);
-        }
-
-        std::vector<std::unordered_map<int, std::tuple<int, int>>> trees(HP.L);
-        int l = 0;
-        while (std::getline(tree_file, line)) {
-            if (line[0] == 'e') { break; }
-            parse_coal_tree(line.c_str(), trees[l], -1);
-            ++l;
-            if (l >= HP.L) { throw std::runtime_error("# trees should match sequence length"); }
-        }
-
+        std::vector<std::unordered_map<int, std::tuple<int, int>>> coal_trees{parse_tree_file(tree_file_name, HP.L)};
         for (int l = 0; l < HP.L; ++l) {
-            if (clusters.rs[l].size() > 64) {
-                throw std::runtime_error("uint64_t insufficient for cluster set bitvector.");
-            };
             std::unordered_map<Cluster*, int> cluster_idxs;
             cluster_idxs.reserve(clusters.rs[l].size());
             int i = 0;
@@ -302,8 +227,7 @@ int main(int argc, char *argv[]) {
                 cluster_idxs.emplace(c, i);
                 ++i;
             }
-            int parsimony = calc_parsimony(-1, l, trees[l], clusters, cluster_idxs).score;
-            int excess_parsimony = parsimony - (clusters.rs[l].size() - 1);
+            int excess_parsimony = calc_excess_parsimony(l, coal_trees[l], clusters, cluster_idxs);
             std::cout << "l=" << l << " excess_parsimony=" << excess_parsimony << '\n';
         }
     }
